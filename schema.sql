7c081922c43dd0cf3bec48eeeb22421033b2a8b1
asalmeron3 <arturosalmeron24@gmail.com>
2024-11-20 16:40:16 -0500

    added menu items and foreign keys to schema

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ CREATE DATABASE cheweats_restaurants;
 USE cheweats_restaurants;
 
 CREATE TABLE restaurants (
-  id integer PRIMARY KEY AUTO_INCREMENT,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(255) NOT NULL,
   shared_rate_per_mile DECIMAL(4,2)
 );
@@ -19,7 +19,7 @@ VALUES
 Select * from restaurants;
 
 CREATE TABLE current_market (
-  id integer PRIMARY KEY AUTO_INCREMENT,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   city_name VARCHAR(20) NOT NULL,
   minimum_pay_rate_for_two_miles DECIMAL(4,2)
 );
@@ -30,7 +30,7 @@ VALUES
 ("Salem", 7);
 
 CREATE TABLE customers (
-  id integer PRIMARY KEY AUTO_INCREMENT,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(40) NOT NULL,
   shared_rate_per_mile DECIMAL(4,2)
 );
@@ -43,3 +43,27 @@ VALUES
 ("Lilia", 2),
 ("Jen", 3.5),
 ("Alice", 1.5);
+
+CREATE TABLE menu_items (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR (255) NOT NULL,
+  price DECIMAL (5,2),
+  restaurant_id INT,
+  FOREIGN KEY (restaurant_id)
+    REFERENCES restaurants(id) 
+    ON UPDATE CASCADE
+    ON DELETE CASCADE 
+);
+
+INSERT INTO menu_items(name, price, restaurant_id)
+VALUES
+("Big Snack", 7.80, 1),
+("Big Fries", 4.25, 1),
+("Mave's Double", 8.99, 2),
+("Frostique", 2.25, 2),
+("Wobbler", 7.99, 3),
+("Nugget", 0.40, 3),
+("Lane's Special", 16, 4),
+("Breadsticks", 4, 4),
+("Regular Roll", 7, 5),
+("Special Roll", 9, 5);
