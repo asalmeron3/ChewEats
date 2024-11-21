6854de2230292b7fd876e7fbcd7944c5a7422cfd
asalmeron3 <arturosalmeron24@gmail.com>
2024-11-21 17:15:55 -0500

    added media queries and styling for viewing changes

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,11 +1,9 @@
 function showRestMenu(restId){
-  $(".menu-box").empty();
+  $(".menu").empty();
   let menu = $("<table>").attr("class", "menu");
 
   $.get("/api/menu-items/"+ restId, function(data){
     data.forEach(e => {
-      //let menu = $("<div>").attr("class", "menu");
-      //let menuItem = $("<div>").attr("class", "menu-item");
       menu.append(`<tr><td> ${e.name}</td><td>${e.price}</td></tr>`);
     });
 
