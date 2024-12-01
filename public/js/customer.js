48ffeed062fdc0d2c35b92c2441b2a457e77822e
asalmeron3 <arturosalmeron24@gmail.com>
2024-12-01 00:10:06 -0500

    WIP adding jquery dialog for customer selection

--- public/js/customer.js
+++ public/js/customer.js
@@ -5,3 +5,18 @@ function getCustomers(){
     });
   });
 };
+
+$("#customer-dialog").dialog({
+  autoOpen: false,
+  modal: true,
+  buttons: {
+    agatha: function(){console.log("Agatha");},
+    rio: function(){console.log("Rio");},
+    teen: function(){console.log("Teen");}
+  }
+});
+
+$("#open-modal").on("click", function(){
+  $("#customer-dialog").dialog("open");
+  console.log("clicked on modal button");
+});
